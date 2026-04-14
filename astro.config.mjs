9b09a8291713395358c62cd057d4f0eadff7c2b6
Jie Gao <gajie@microsoft.com>
2026-04-14 14:54:46 +0800

    fix: disable smartypants, fix bold markdown in Figma article

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    smartypants: false,
+  },
   vite: {
     plugins: [tailwindcss()]
   }
